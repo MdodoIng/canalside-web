58eccb7b23afacf619bdf358e24ce6f996bdf026
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-10-09 10:10:33 +0530

    fad

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,16 +1,15 @@
-
-
 gsap.registerPlugin(ScrollTrigger);
 
 
-gsap.to(".imageClip", {
-  duration: 2,
-  clipPath: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)",
-  scrollTrigger: {
-    trigger: ".imageClip",
-    start: "top 80%",     
-    end: "top 30%",       
-    scrub: .5,          
-    
-  }
+gsap.utils.toArray(".imageClip").forEach((image) => {
+  gsap.to(image, {
+    ease: "linear",
+    clipPath: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)",
+    scrollTrigger: {
+      trigger: image,
+      start: "bottom 90%", 
+      end: "bottom top", 
+      scrub: 1,
+    },
+  });
 });
