e921097d66e6169e261e9718c3e23d2b0c7eb018
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-10-09 10:55:49 +0530

    gds

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -3,13 +3,15 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.utils.toArray(".imageClip").forEach((image) => {
   gsap.to(image, {
-    ease: "linear",
+    ease: "power2.out",
     clipPath: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)",
+    
     scrollTrigger: {
       trigger: image,
       start: "bottom 90%", 
       end: "bottom top", 
-      scrub: 1,
+      scrub: 0.5,    
+      invalidateOnRefresh: true,
     },
   });
 });
