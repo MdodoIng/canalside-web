c60e8c8b24c233128e9da566dac1a02c70289bc8
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-10-09 16:44:24 +0530

    fds

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -15,3 +15,18 @@ gsap.utils.toArray(".imageClip").forEach((image) => {
     },
   });
 });
+
+gsap.utils.toArray(".textClip").forEach((text) => {
+  gsap.to(text, {
+    ease: "power2.out",
+    clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+    
+    scrollTrigger: {
+      trigger: text,
+      start: "bottom bottom", 
+      end: "bottom 90%", 
+      scrub: 0.5,    
+      invalidateOnRefresh: true,
+    },
+  });
+});
