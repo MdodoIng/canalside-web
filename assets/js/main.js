a0ff8d2b9cfe7be9eb718a2fd379f51cdf13f8a9
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-10-10 12:09:16 +0530

    asf

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,35 @@
 import "./animations.js";
+
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start) + "+";
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll(".counter");
+
+  if (counters) {
+    counters.forEach((element) => {
+
+      
+      const count = Number(element.innerHTML.replace("+",""))
+      const observer = new IntersectionObserver((entries) => {
+       
+        if (entries[0].isIntersecting) {
+          animateValue(element, 0, count, 2000);
+        }
+      }, {
+        threshold: 1.0,
+      });
+      observer.observe(element);
+    });
+  }
+});
