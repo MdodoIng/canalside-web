6905717886b685700e7b9221f0949be340114761
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-10-21 18:01:44 +0530

    sfas

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,18 +18,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (counters) {
     counters.forEach((element) => {
-
-      
-      const count = Number(element.innerHTML.replace("+",""))
-      const observer = new IntersectionObserver((entries) => {
-       
-        if (entries[0].isIntersecting) {
-          animateValue(element, 0, count, 2000);
+      const count = Number(element.innerHTML.replace("+", ""));
+      const observer = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            animateValue(element, 0, count, 2000);
+          }
+        },
+        {
+          threshold: 1.0,
         }
-      }, {
-        threshold: 1.0,
-      });
+      );
       observer.observe(element);
     });
   }
+
+  window.onload = function () {
+    if (window.location.pathname === "/") {
+      // Select header element
+      const header = document.getElementsByTagName("header")[0];
+
+      
+      header.className += " bg-gray";
+    }
+  };
 });
